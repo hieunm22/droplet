661598cd9bb499ac96b2d43a3607422bef1a829a
Ming Hieu <hieunm1987@gmail.com>
2020-03-29 20:29:50 +0700

    Add files via upload
    edit install commands

--- sql_server_ubuntu.sh
+++ sql_server_ubuntu.sh
@@ -1,19 +1,13 @@
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2019.list)"
 apt-get update
+apt-get -y upgrade
+sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add –
+add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)"
+apt-get update
+apt install libcurl3
 apt-get install -y mssql-server
 /opt/mssql/bin/mssql-conf setup
-ufw allow 22
-ufw allow 1433
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | tee /etc/apt/sources.list.d/msprod.list
-apt-get update
-apt-get install mssql-tools unixodbc-dev
-apt-get install mssql-tools
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-source ~/.bashrc
-
-
-
-systemctl status mssql-server --no-pager
+2
+yes
+Quynhhoa@12
+Quynhhoa@12
+systemctl status mssql-server.service
