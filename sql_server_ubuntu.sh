870f453ef4a95501c7827c17c69000c7234b7dd1
Ming Hieu <hieunm1987@gmail.com>
2020-03-29 23:31:57 +0700

    Update sql_server_ubuntu.sh

--- sql_server_ubuntu.sh
+++ sql_server_ubuntu.sh
@@ -4,11 +4,3 @@ sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key 
 add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)"
 apt-get update
 apt install libcurl3
-y
-apt-get install -y mssql-server
-/opt/mssql/bin/mssql-conf setup
-2
-yes
-123456aA@
-123456aA@
-systemctl status mssql-server.service
