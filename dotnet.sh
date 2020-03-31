f28ecba26f8b33bf7afdd78a0e4550398ca2fece
Ming Hieu <hieunm1987@gmail.com>
2020-03-31 10:42:22 +0700

    Add files via upload
    dotnet script

---
+++ dotnet.sh
@@ -0,0 +1,6 @@
+wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-3.1
