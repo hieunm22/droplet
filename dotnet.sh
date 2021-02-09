f7fffc290b8c91f6e5a7d023087c3a896e8b193b
Ming Hieu <hieunm1987@gmail.com>
2021-02-10 01:01:49 +0700

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -7,3 +7,13 @@ sudo apt-get update
 sudo apt-get install dotnet-sdk-2.2
 sudo apt-get install dotnet-sdk-3.1
 
+###############
+
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo add-apt-repository universe
+sudo apt-get install apt-transport-https
+
+sudo apt-get update
+sudo apt-get install dotnet-sdk-3.1
